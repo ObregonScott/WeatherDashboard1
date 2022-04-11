dbbe517603fd0a1e0798d24e3cc93192ec48324b
obregonscott <obregon.scott@gmail.com>
2022-04-11 18:02:05 -0500

    added local storage

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 var searchForm = document.querySelector("#searchForm");
+
 var historyArray = JSON.parse(localStorage.getItem("history")) ?JSON.parse(localStorage.getItem("history")):[]
+
 searchForm.addEventListener("click", function(){
     console.log("hit")
 var cityInput = document.querySelector("#cityInput").value
 historyArray.push(cityInput)
+
+
 localStorage.setItem("history", JSON.stringify(historyArray))
 for (let i = 0; i < historyArray.length; i++) {
-    console.log(historyArray [i])
+    
 }
 
 
 
 });
-
