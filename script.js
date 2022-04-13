d462745e4f63547632f1e232acee66ee4ec10747
obregonscott <obregon.scott@gmail.com>
2022-04-12 19:30:31 -0500

    trying to display local storage

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 var searchForm = document.querySelector("#searchForm");
+var historyArray = JSON.parse(localStorage.getItem("history")) ? JSON.parse(localStorage.getItem("history")) : []
+var prevCity = document.querySelector("#prevCity")
 
-var historyArray = JSON.parse(localStorage.getItem("history")) ?JSON.parse(localStorage.getItem("history")):[]
-
-searchForm.addEventListener("click", function(){
+searchForm.addEventListener("click", function () {
     console.log("hit")
-var cityInput = document.querySelector("#cityInput").value
-historyArray.push(cityInput)
+    var cityInput = document.querySelector("#cityInput").value
+    historyArray.push(cityInput)
 
-localStorage.setItem("history", JSON.stringify(historyArray))
-for (let i = 0; i < historyArray.length; i++) {
-}
+    localStorage.setItem("history", JSON.stringify(historyArray))
+    for (let i = 0; i < historyArray.length; i++) {
+    }
 
 });
+
+searchForm.addEventListener("click", insertCity)
+function insertCity(event) {
+    event.preventDefault();
+    var cityListEl = document.createElement("button")
+    cityListEl.textContent = cityInput.value;
+    cityInput.className = "list-group-item";
+    prevCity.appendChild(cityListEl)
+}
+
+;
